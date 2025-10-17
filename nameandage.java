e2e36826ad4d89a5d2e2d437f8a0058d6e872839
Manish-7051 <manishsharmajanyal@gmail.com>
2025-10-17 21:14:55 +0530

    Create nameandage.java

---
+++ nameandage.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+public class nameandage
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter a name");
+	    String name=sc.nextLine();
+	    System.out.println("Enter a age:");
+	    int age=sc.nextInt();
+	    
+	 
+	   
+	    
+	    
+		System.out.println(" My name is "+name + " age is "+age);
+	}
+}
