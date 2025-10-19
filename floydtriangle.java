9e9747d7a6634ee10272b4ff2d3fb8fddbd0fc2d
Manish-7051 <manishsharmajanyal@gmail.com>
2025-10-19 18:20:07 +0530

    Create floydtriangle.java

---
+++ floydtriangle.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter number of rows");
+	    int n=sc.nextInt();
+	    
+	    int num=1;
+	    System.out.println("----Floyd's Triangle----");
+	    for(int i=1;i<=n;i++){
+	        for(int j=1;j<=i;j++){
+	   
+	            
+	              
+	     System.out.print(num+" ");
+	     num++;
+	   }
+	        
+	        
+	        System.out.println();
+	    
+	  
+
+	    
+	        
+	      
+	        
+	        
+	        
+	        
+	    }
+	    
+	  
+		
+	}
+}
